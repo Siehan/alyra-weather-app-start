938348a2714aaec159e881cceaec02bc472350b7
Siehan <memain.ye.sylvie@gmail.com>
2022-02-24 22:13:44 +0100

    modification text color

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,5 +1,7 @@
+// Description of the temperature: overcast, cloudy...
+
 const Description = ({ description }) => {
-  return <p>{description}</p>;
+  return <h4 className="text-warning pb-3 text-capitalize">{description}</h4>;
 };
 
 export default Description;
