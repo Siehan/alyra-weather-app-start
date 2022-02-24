1602c2c040e1c629471da18725125f0c8c07abd8
Siehan <memain.ye.sylvie@gmail.com>
2022-02-24 22:23:49 +0100

    modification icon size

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,5 +1,5 @@
 const Icon = ({ iconID }) => {
-  return !!iconID && <img src={`https://openweathermap.org/img/wn/${iconID}@4x.png`} alt="" width="100" height="100" />;
+  return !!iconID && <img src={`https://openweathermap.org/img/wn/${iconID}@4x.png`} alt="" width="150" height="150" />;
 };
 
 export default Icon;
